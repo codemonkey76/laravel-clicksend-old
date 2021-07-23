2f07797a66bf001050bd3fa43715bc72d5566908
Shane Poppleton <shane@bjja.com.au>
2021-07-23 12:19:25 +1000

    Add code to send SMS

--- src/ClickSend.php
+++ src/ClickSend.php
@@ -3,14 +3,14 @@
 namespace Codemonkey76\ClickSend;
 
 use ClickSend\Api\SMSApi;
+use ClickSend\ApiException;
 use ClickSend\Model\SmsMessage;
+use ClickSend\Model\SmsMessageCollection;
 use GuzzleHttp\Client;
 use Illuminate\Contracts\Config\Repository as Config;
 use ClickSend\Configuration;
 class ClickSend
 {
-    protected string $from = '';
-    protected string $to = '';
     protected SMSApi $apiInstance;
     protected SmsMessage $sms_message;
 
@@ -21,18 +21,42 @@ class ClickSend
 
     public function __construct(Config $config)
     {
-        $apiInstance = new SMSApi(
+        $this->apiInstance = new SMSApi(
             new Client(),
             Configuration::getDefaultconfiguration()
                 ->setUsername($config['clicksend.username'])
                 ->setPassword($config['clicksend.password'])
         );
-        //$sms_message = new SmsMessage()
+        $this->sms_message = new SmsMessage();
     }
-    public function message(string $message)
+
+    public function from(string $from): self
     {
+        $this->sms_message->setFrom($from);
 
+        return $this;
     }
 
-    // Build your next great package.
+    public function to(string $to): self
+    {
+        $this->sms_message->setTo($to);
+
+        return $this;
+    }
+
+    public function message(string $message): bool
+    {
+        $this->sms_message->setBody($message);
+
+        try {
+            $result = $this->apiInstance->smsSendPost(new SmsMessageCollection(['message' => [$this->sms_message]]));
+            info($result);
+
+        } catch (ApiException $ex) {
+            info($ex->getMessage());
+
+            return false;
+        }
+        return true;
+    }
 }
